e3956c93f4422c84ab30255fb3ed5d0ebfe28cdd
Ahmad Erfani <ahmaderfani12@gmail.com>
2021-04-28 00:52:40 +0430

    Read color data from texture

--- Assets/Project/Shaders/PointGPU.hlsl
+++ Assets/Project/Shaders/PointGPU.hlsl
@@ -1,32 +1,38 @@
 #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
 	StructuredBuffer<float3> _Positions;
+	StructuredBuffer<float3> _uvs;
 #endif
 
 float _Step;
 float4 _color;
 float1 _intensity;
+float3 _worldPos;
+float1 _scale;
+float3 _uvPosition;
 
-void ConfigureProcedural () {
+void ConfigureProcedural () { //run per vertex
 	#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
 		float3 position = _Positions[unity_InstanceID];
-
+	    _uvPosition = _uvs[unity_InstanceID];
 		unity_ObjectToWorld = 0.0;
-		unity_ObjectToWorld._m03_m13_m23_m33 = float4(position, 1.0);
+		unity_ObjectToWorld._m03_m13_m23_m33 = float4((position * _scale) + _worldPos, 1.0);
 		unity_ObjectToWorld._m00_m11_m22 = _Step ;
-	#endif
+#endif
 }
 
-void ShaderGraphFunction_float(float3 In, out float3 Out, out float4 Color, out float1 Intensity)
+void ShaderGraphFunction_float(float3 In, out float3 Out, out float4 Color, out float1 Intensity , out float3 UVPosition)
 {
 	Out = In;
     Color = _color;
     Intensity = _intensity;
+    UVPosition = _uvPosition;
 
 }
 
-void ShaderGraphFunction_half(half3 In, out half3 Out, out float4 Color, out float1 Intensity)
+void ShaderGraphFunction_half(half3 In, out half3 Out, out float4 Color, out float1 Intensity, out float3 UVPosition)
 {
 	Out = In;
     Color = _color;
     Intensity = _intensity;
+    UVPosition = _uvPosition;
 }
--- Assets/Project/SingelMeshView/MeshGraph.cs
+++ Assets/Project/SingelMeshView/MeshGraph.cs
@@ -11,40 +11,65 @@ public class MeshGraph : MonoBehaviour
 
     [SerializeField] Material material = default;
 
-    [SerializeField] Mesh mesh = default;
+    [SerializeField] Mesh pointsMesh = default;
 
     [SerializeField, Range(0.001f, 0.2f)] float step = 0.02f;
 
     [SerializeField] Color pointsColor = Color.white;
 
-    [SerializeField, Range(0.0f, 1.0f)] float colorIntensity = 0;
+    [SerializeField, Range(0.0f, 1.0f)] float colorIntensity = 0.5f;
+    [SerializeField, Range(0.0f, 1.0f)] float colorFromTextureLerp = 0;
+    [SerializeField] Texture2D sourceMeshTexture = default;
+    [SerializeField] private bool useUvs = false;
 
     ComputeBuffer positionsBuffer;
+    ComputeBuffer uvsBuffer;
 
-    int positionsId;
+
     Bounds bounds;
     Vector3[] vertices;
+    Vector2[] uvs;
+
 
     private void Awake()
     {
-         positionsId = Shader.PropertyToID("_Positions");
+        SetPositionBufferToComputeShader();
 
-        vertices = sourceMesh.vertices;
-        Debug.Log("vertis Count :" + vertices.Length);
-
-        positionsBuffer = new ComputeBuffer(vertices.Length, 3 * 4);
-        positionsBuffer.SetData(vertices);
-
-        material.SetBuffer(positionsId, positionsBuffer);
-
-        computeShader.SetBuffer(0, "_Positions", positionsBuffer);
-
-       int groups = Mathf.CeilToInt(vertices.Length / 64f);
-      
-        computeShader.Dispatch(0, groups, 1, 1);
+        SetStaticMaterialData();
+        DispachComputeShader();
 
         bounds = new Bounds(Vector3.zero, Vector3.one * 200);
     }
+
+    private void SetPositionBufferToComputeShader()
+    {
+        vertices = sourceMesh.vertices;
+        Debug.Log("vertis Count :" + vertices.Length);
+        positionsBuffer = new ComputeBuffer(vertices.Length, 3 * 4);
+        positionsBuffer.SetData(vertices);
+    }
+
+    private void SetStaticMaterialData()
+    {
+        material.SetBuffer("_Positions", positionsBuffer);
+        colorFromTextureLerp = 0;
+        if (useUvs)
+        {
+            uvs = sourceMesh.uv;
+            uvsBuffer = new ComputeBuffer(uvs.Length, 2 * 4);
+            uvsBuffer.SetData(uvs);
+            material.SetBuffer("_uvs", uvsBuffer);
+            material.SetTexture("_MainTex", sourceMeshTexture);
+            colorFromTextureLerp = 1;
+        }
+    }
+
+    private void DispachComputeShader()
+    {
+        int groups = Mathf.CeilToInt(vertices.Length / 64f);
+        computeShader.Dispatch(0, groups, 1, 1);
+    }
+
 
     private void Update()
     {
@@ -54,20 +79,31 @@ public class MeshGraph : MonoBehaviour
 
     private void DrawInsyanceMeshes()
     {
-        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
+        Graphics.DrawMeshInstancedProcedural(pointsMesh, 0, material, bounds, positionsBuffer.count);
     }
 
+   
     private void SetMaterialDynamicData()
     {
         material.SetFloat("_Step", step);
+        material.SetFloat("_scale", this.transform.localScale.x);
         material.SetFloat("_intensity", colorIntensity);
+        material.SetVector("_worldPos", this.transform.position);
         material.SetVector("_color", new Vector4(pointsColor.r, pointsColor.g, pointsColor.b, 1));
+        material.SetFloat("_ColorFromTextureLerp", colorFromTextureLerp);
     }
 
     void OnDisable()
     {
         positionsBuffer.Release();
         positionsBuffer = null;
+        if (useUvs)
+        {
+            uvsBuffer.Release();
+            uvsBuffer = null;
+        }
+
     }
+
 
 }
